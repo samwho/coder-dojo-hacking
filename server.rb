bb7a1d97e697d861131e5649c5344e208b3f9130
Sam Rose <samwho@lbak.co.uk>
2012-09-15 12:03:18 +0100

    Server skeleton.

--- server.rb
+++ server.rb
@@ -4,3 +4,38 @@ require 'haml'
 get '/' do
   haml :index
 end
+
+# HTML comment stage
+get '/requestforcomment' do
+  haml :htmlcomment
+end
+
+# Large leaked file full of hashed passwords
+get '/needleinahaystack' do
+  haml :leakedfile
+end
+
+# Hidden in an image
+get '/nowyouseemenowyoudont' do
+  haml :hiddenimage
+end
+
+# Book cipher
+get '/onceuponatime' do
+  haml :bookcipher
+end
+
+# Encrypted message in headers
+get '/viewfromthetop' do
+  haml :headers
+end
+
+# Binary on/off password with check boxes
+get '/0110' do
+  haml :binary
+end
+
+# JS function to run in the console
+get '/funktion' do
+  haml :function
+end
